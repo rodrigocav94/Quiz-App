26e38f3d1403de19c7d7bee0f5c479ec9ebf1e9e
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-09-29 19:15:50 -0300

    Refactored QuizView to be easier to read

--- Quiz App/Scenes/Quiz/QuizView.swift
+++ Quiz App/Scenes/Quiz/QuizView.swift
@@ -13,76 +13,21 @@ struct QuizView: View {
     @StateObject var vm = QuizViewModel()
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            HStack {
-                BackButton()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .overlay(alignment: .trailing) {
-                        HStack(spacing: 3) {
-                            Image(systemName: "clock.fill")
-                                .foregroundStyle(.quizGreen)
-                            Text(vm.remainingTimeString)
-                                .frame(width: 40)
-                        }
-                        .capsuleViewStyle(fontColor: .quizOffBlack, borderColor: .quizOffWhite)
-                    }
-            }
+            BackButtonAndClock
             
             HStack(spacing: 20) {
-                HStack(spacing: 3) {
-                    Image(systemName: "checkmark.seal.fill")
-                    Text("\(vm.score)")
-                }
-                .bold()
-                .padding(.horizontal, 10)
-                .padding(.vertical, 2)
-                .background {
-                    Capsule()
-                        .fill(.quizGreen)
-                }
-                .foregroundStyle(.white)
-                
-                HStack(spacing: 3) {
-                    ForEach(vm.questions.indices, id: \.self) { step in
-                        Capsule()
-                            .frame(height: 4)
-                            .foregroundStyle(step > vm.questionIndex ? .quizOffWhite.opacity(0.5) : .quizGreen)
-                    }
-                }
-                .animation(.default, value: vm.questionIndex)
+                CorrectAnswerTracker
+                QuestionIndex
             }
             
             VStack(alignment: .leading, spacing: 20) {
-                Text(vm.currentQuestion.statement)
-                    .font(Font.custom("Kickers-Regular", size: 65))
-                    .minimumScaleFactor(0.1)
-                
-                VStack(spacing: 10) {
-                    ForEach(Array(vm.currentQuestion.options.enumerated()), id: \.element) { (index, element) in
-                        Button {
-                            vm.answer(element)
-                        } label: {
-                            Text(element)
-                        }
-                        .buttonStyle(QuizOptionButtonStyle(answer: element, selectedOption: vm.selectedOption, didAnswerCorrectly: vm.didAnswerCorrectly))
-                    }
-                }
-                .disabled(vm.selectedOption != nil)
-                .layoutPriority(1)
-                .transition(.slide.combined(with: .opacity))
-                .id(vm.currentQuestion)
-                .frame(maxHeight: .infinity, alignment: .bottom)
+                QuestionStatement
+                QuestionOptions
             }
             .animation(.default, value: vm.currentQuestion)
             .padding(.bottom)
             .background(alignment: .topTrailing) {
-                Image("quiz-background")
-                    .renderingMode(.template)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxHeight: 150)
-                    .foregroundStyle(.white)
-                    .padding(.vertical)
-                    .offset(x: 40)
+                Illustration
             }
         }
         .padding(.horizontal)
@@ -101,6 +46,108 @@ struct QuizView: View {
     }
 }
 
+// MARK: - Back Button And Clock
+extension QuizView {
+    var Clock: some View {
+        HStack(spacing: 3) {
+            Image(systemName: "clock.fill")
+                .foregroundStyle(.quizGreen)
+            Text(vm.remainingTimeString)
+                .frame(width: 40)
+        }
+        .capsuleViewStyle(
+            fontColor: .quizOffBlack,
+            borderColor: .quizOffWhite
+        )
+    }
+    
+    var BackButtonAndClock: some View {
+        HStack {
+            BackButton()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .overlay(alignment: .trailing) {
+                    Clock
+                }
+        }
+    }
+}
+
+// MARK: - Correct Answer Tracker
+extension QuizView {
+    var CorrectAnswerTracker: some View {
+        HStack(spacing: 3) {
+            Image(systemName: "checkmark.seal.fill")
+            Text("\(vm.score)")
+        }
+        .bold()
+        .padding(.horizontal, 10)
+        .padding(.vertical, 2)
+        .background {
+            Capsule()
+                .fill(.quizGreen)
+        }
+        .foregroundStyle(.white)
+    }
+}
+
+// MARK: - Question Index
+extension QuizView {
+    var QuestionIndex: some View {
+        HStack(spacing: 3) {
+            ForEach(vm.questions.indices, id: \.self) { step in
+                Capsule()
+                    .frame(height: 4)
+                    .foregroundStyle(step > vm.questionIndex ? .quizOffWhite.opacity(0.5) : .quizGreen)
+            }
+        }
+        .animation(.default, value: vm.questionIndex)
+    }
+}
+
+// MARK: - Question Statement
+extension QuizView {
+    var QuestionStatement: some View {
+        Text(vm.currentQuestion.statement)
+            .font(Font.custom("Kickers-Regular", size: 65))
+            .minimumScaleFactor(0.1)
+        
+    }
+}
+
+// MARK: - Question Options
+extension QuizView {
+    var QuestionOptions: some View {
+        VStack(spacing: 10) {
+            ForEach(Array(vm.currentQuestion.options.enumerated()), id: \.element) { (index, element) in
+                Button {
+                    vm.answer(element)
+                } label: {
+                    Text(element)
+                }
+                .buttonStyle(QuizOptionButtonStyle(answer: element, selectedOption: vm.selectedOption, didAnswerCorrectly: vm.didAnswerCorrectly))
+            }
+        }
+        .disabled(vm.selectedOption != nil)
+        .layoutPriority(1)
+        .transition(.slide.combined(with: .opacity))
+        .id(vm.currentQuestion)
+        .frame(maxHeight: .infinity, alignment: .bottom)
+    }
+}
+
+// MARK: - Illustration
+extension QuizView {
+    var Illustration: some View {
+        Image("quiz-background")
+            .renderingMode(.template)
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: 150)
+            .foregroundStyle(.white)
+            .padding(.vertical)
+            .offset(x: 40)
+    }
+}
 #Preview {
     QuizView()
 }
