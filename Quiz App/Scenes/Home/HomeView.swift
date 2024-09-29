f17ceefdf169bc48089be7958a5f16211ad89cf9
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-09-29 19:22:10 -0300

    Refactored home view to be easier to read

--- Quiz App/Scenes/Home/HomeView.swift
+++ Quiz App/Scenes/Home/HomeView.swift
@@ -25,70 +25,14 @@ struct HomeView: View, Navigator {
             cachedNetworkResults.fetchQuestions()
         }
     }
+    
     var contentBody: some View {
         VStack(spacing: 15) {
-            NavigationLink {
-                RankingView()
-            } label: {
-                Text("Hall da Fama")
-                    .capsuleViewStyle(fontColor: .quizOffBlack, borderColor: .quizGreen)
-            }
-            .padding(.trailing)
-            .frame(height: 44)
-            .frame(maxWidth: .infinity, alignment: .trailing)
-            
-            Group {
-                Text("Quem está por aqui?")
-                    .font(Font.custom("Kickers-Regular", size: 55))
-                    .minimumScaleFactor(0.6)
-                Text("Já faz parte da turma ou está criando uma perfil agora?")
-                    .foregroundStyle(.secondary)
-            }
-            .padding(.horizontal, 60)
-            
-            TabView(selection: $vm.selectedProfile) {
-                ForEach(profiles) { profile in
-                    ProfileSelection(homeVM: vm, profile: profile)
-                        .tag(profile as Profile?)
-                }
-                ProfileSelection(homeVM: vm)
-                    .tag(nil as Profile?)
-            }
-            .tabViewStyle(.page(indexDisplayMode: .never))
-            .onAppear {
-                vm.updateProfileSelection(withQuery: profiles)
-            }
-            
+            RankingLink
+            Header
+            ProfileSelectionTab
             TabViewIndex(currentIndex: vm.getProfileIndex(query: profiles), total:  profiles.count + 1)
-            
-            Group {
-                if (vm.hasError) && vm.didNotLoadYet {
-                    Button("Tentar Novamente") {
-                        vm.tryLoadingAgain()
-                    }
-                    .onAppear {
-                        vm.displayingAlert = true
-                    }
-                } else {
-                    NavigationLink {
-                        QuizView(profile: vm.selectedProfile)
-                    } label: {
-                        HStack(spacing: 10) {
-                            if vm.didNotLoadYet {
-                                ProgressView()
-                                    .tint(.white)
-                                    .scaleEffect(x: 1.25, y: 1.25)
-                            }
-                            Text(vm.didNotLoadYet ? "Carregando..." : "Começar Quiz")
-                        }
-                    }
-                }
-            }
-            .buttonStyle(LargeButtonStyle())
-            .padding(.horizontal, 60)
-            .padding(.bottom)
-            .disabled(vm.cantStartQuiz)
-            
+            BottomButton
         }
         .foregroundStyle(.quizOffBlack)
         .multilineTextAlignment(.center)
@@ -100,6 +44,96 @@ struct HomeView: View, Navigator {
         .alert(isPresented: $vm.displayingAlert) {
             Alert(title: Text("Ops! Não conseguimos sincronizar as perguntas."), message: Text("Tente novamente em alguns instantes."))
         }
+    }
+}
+
+// MARK: - Ranking Link
+extension HomeView {
+    var RankingLink: some View {
+        NavigationLink {
+            RankingView()
+        } label: {
+            Text("Hall da Fama")
+                .capsuleViewStyle(fontColor: .quizOffBlack, borderColor: .quizGreen)
+        }
+        .padding(.trailing)
+        .frame(height: 44)
+        .frame(maxWidth: .infinity, alignment: .trailing)
+    }
+}
+
+// MARK: - Header
+extension HomeView {
+    var Header: some View {
+        Group {
+            Text("Quem está por aqui?")
+                .font(Font.custom("Kickers-Regular", size: 55))
+                .minimumScaleFactor(0.6)
+            Text("Já faz parte da turma ou está criando uma perfil agora?")
+                .foregroundStyle(.secondary)
+        }
+        .padding(.horizontal, 60)
+    }
+}
+
+// MARK: - Profile Selection Tab
+extension HomeView {
+    var ProfileSelectionTab: some View {
+        TabView(selection: $vm.selectedProfile) {
+            ForEach(profiles) { profile in
+                ProfileSelection(homeVM: vm, profile: profile)
+                    .tag(profile as Profile?)
+            }
+            ProfileSelection(homeVM: vm)
+                .tag(nil as Profile?)
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .onAppear {
+            vm.updateProfileSelection(withQuery: profiles)
+        }
+    }
+}
+
+// MARK: - Bottom Button
+extension HomeView {
+    // Try Again Button
+    var TryAgainButton: some View {
+        Button("Tentar Novamente") {
+            vm.tryLoadingAgain()
+        }
+        .onAppear {
+            vm.displayingAlert = true
+        }
+    }
+    
+    // Quiz Link
+    var QuizLink: some View {
+        NavigationLink {
+            QuizView(profile: vm.selectedProfile)
+        } label: {
+            HStack(spacing: 10) {
+                if vm.didNotLoadYet {
+                    ProgressView()
+                        .tint(.white)
+                        .scaleEffect(x: 1.25, y: 1.25)
+                }
+                Text(vm.didNotLoadYet ? "Carregando..." : "Começar Quiz")
+            }
+        }
+    }
+    
+    var BottomButton: some View {
+        Group {
+            if (vm.hasError) && vm.didNotLoadYet {
+                TryAgainButton
+            } else {
+                QuizLink
+            }
+        }
+        .buttonStyle(LargeButtonStyle())
+        .padding(.horizontal, 60)
+        .padding(.bottom)
+        .disabled(vm.cantStartQuiz)
     }
 }
 
