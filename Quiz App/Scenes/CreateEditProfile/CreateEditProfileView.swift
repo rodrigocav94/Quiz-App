e6c4e5cfc2c94f4d272f83766a77246d23997fd6
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-09-29 19:02:45 -0300

    Refactored Create Edit Profile Screen to be easier to read

--- Quiz App/Scenes/CreateEditProfile/CreateEditProfileView.swift
+++ Quiz App/Scenes/CreateEditProfile/CreateEditProfileView.swift
@@ -20,45 +20,10 @@ struct CreateEditProfileView: View {
     var body: some View {
         VStack(spacing: 0) {
             if !keyboardShowing {
-                Text("Mostre quem você é!")
-                    .font(Font.custom("Kickers-Regular", size: 50))
-                    .multilineTextAlignment(.center)
-                HStack {
-                    arrowButton(goesToTheLeft: true)
-                    
-                    Image("pfp\(vm.pfpIndex)")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(maxWidth: 220)
-                    
-                    arrowButton(goesToTheLeft: false)
-                }
-                .background(
-                    SpinningLight().scaleEffect(x: 2, y: 2)
-                        .allowsHitTesting(false)
-                )
-                .frame(maxHeight: .infinity)
+                Header
+                ProfileIconSelection
             }
-            
-            VStack {
-                TextField("Seu Nome", text: $vm.name)
-                    .font(Font.custom("Kickers-Regular", size: 40))
-                    .multilineTextAlignment(.center)
-                    .lineLimit(0)
-                    .focused($textFieldIsFocused)
-                
-                Button(keyboardShowing ? "Voltar" : "Guardar") {
-                    if keyboardShowing {
-                        textFieldIsFocused = false
-                    } else {
-                        vm.onSave(context: modelContext, profile: profile)
-                    }
-                }
-                .buttonStyle(LargeButtonStyle(color: .quizGreen))
-            }
-            .padding()
-            .background(.quizOffWhite)
-            .clipShape(RoundedRectangle(cornerRadius: 25))
+            TextFieldAndButton
         }
         .padding(.horizontal, 40)
         .padding(.bottom)
@@ -66,20 +31,7 @@ struct CreateEditProfileView: View {
         .frame(maxHeight: .infinity)
         .background(.quizYellow)
         .safeAreaInset(edge: .top) {
-            BackButton()
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-                .overlay(alignment: .trailing) {
-                    if let profile {
-                        Button{
-                            vm.onDelete(context: modelContext, profile: profile, homeVM: homeVM)
-                        } label: {
-                            Text("Remover")
-                                .capsuleViewStyle(fontColor: .quizOffBlack, borderColor: .red)
-                        }
-                        .padding(.trailing)
-                    }
-                }
+            BackButtonAndDelete
         }
         .navigationBarBackButtonHidden()
         .onAppear {
@@ -91,6 +43,40 @@ struct CreateEditProfileView: View {
                 message: Text("Por favor, insira um nome para continuar.")
             )
         }
+    }
+}
+
+#Preview {
+    CreateEditProfileView(homeVM: HomeViewModel())
+}
+
+// MARK: - Header
+extension CreateEditProfileView {
+    var Header: some View {
+        Text("Mostre quem você é!")
+            .font(Font.custom("Kickers-Regular", size: 50))
+            .multilineTextAlignment(.center)
+    }
+}
+
+// MARK: - Profile Icon Selection
+extension CreateEditProfileView {
+    var ProfileIconSelection: some View {
+        HStack {
+            arrowButton(goesToTheLeft: true)
+            
+            Image("pfp\(vm.pfpIndex)")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 220)
+            
+            arrowButton(goesToTheLeft: false)
+        }
+        .background(
+            SpinningLight().scaleEffect(x: 2, y: 2)
+                .allowsHitTesting(false)
+        )
+        .frame(maxHeight: .infinity)
     }
     
     func arrowButton(goesToTheLeft: Bool) -> some View {
@@ -108,6 +94,47 @@ struct CreateEditProfileView: View {
     }
 }
 
-#Preview {
-    CreateEditProfileView(homeVM: HomeViewModel())
+// MARK: - Name TextField And Save Button
+extension CreateEditProfileView {
+    var TextFieldAndButton: some View {
+        VStack {
+            TextField("Seu Nome", text: $vm.name)
+                .font(Font.custom("Kickers-Regular", size: 40))
+                .multilineTextAlignment(.center)
+                .lineLimit(0)
+                .focused($textFieldIsFocused)
+            
+            Button(keyboardShowing ? "Voltar" : "Guardar") {
+                if keyboardShowing {
+                    textFieldIsFocused = false
+                } else {
+                    vm.onSave(context: modelContext, profile: profile)
+                }
+            }
+            .buttonStyle(LargeButtonStyle(color: .quizGreen))
+        }
+        .padding()
+        .background(.quizOffWhite)
+        .clipShape(RoundedRectangle(cornerRadius: 25))
+    }
+}
+
+// MARK: - Back Button And Delete
+extension CreateEditProfileView {
+    var BackButtonAndDelete: some View {
+        BackButton()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal)
+            .overlay(alignment: .trailing) {
+                if let profile {
+                    Button{
+                        vm.onDelete(context: modelContext, profile: profile, homeVM: homeVM)
+                    } label: {
+                        Text("Remover")
+                            .capsuleViewStyle(fontColor: .quizOffBlack, borderColor: .red)
+                    }
+                    .padding(.trailing)
+                }
+            }
+    }
 }
