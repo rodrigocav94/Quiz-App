3be66a68ed3f0a24a416f7a87637700ba9248e85
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-09-29 18:57:36 -0300

    Refactored ranking view to be easier to read

--- Quiz App/Scenes/Ranking/RankingView.swift
+++ Quiz App/Scenes/Ranking/RankingView.swift
@@ -21,52 +21,54 @@ struct RankingView: View {
                 .padding(.horizontal)
             ScrollView(.vertical) {
                 LazyVStack {
-                    Image("ranking-background")
-                        .resizable()
-                        .scaledToFit()
-                        .background(
-                            SpinningLight()
-                        )
-                    Text("Hall da Fama")
-                        .font(Font.custom("Kickers-Regular", size: 55))
-                    Text("Descubra quem brilhou no quiz!")
-                        .padding(.bottom, 20)
-                    
+                    Header
                     if profiles.isEmpty {
-                        Text("Cri cri cri... Ainda não temos ninguém no Hall da Fama. Crie um perfil e responda algumas perguntas para ver seu nome por aqui!")
-                            .foregroundStyle(.secondary)
-                            .multilineTextAlignment(.center)
-                            .padding(.horizontal)
+                        EmptyRanking
                     } else {
-                        ForEach(Array(profiles.enumerated()), id: \.element.id) { (index, profile) in
-                            HStack(spacing: 20) {
-                                Image("pfp\(profile.icon)")
-                                    .resizable()
-                                    .scaledToFit()
-                                Text("\(index + 1). \(profile.name)")
-                                    .font(Font.custom("Kickers-Regular", size: 25))
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .lineLimit(1)
-                                
-                                Text("\(profile.maxScore) acertos")
-                                    .font(Font.custom("Kickers-Regular", size: 25))
-                            }
-                            .padding(.trailing)
-                            .frame(height: 50)
-                            .background(Capsule().stroke(.quizOffWhite, lineWidth: 2))
-                        }
+                        RankingCells
                     }
                 }
                 .padding([.horizontal, .bottom])
             }
-            .mask(gradientMask)
+            .mask(GradientMask)
         }
         .foregroundStyle(.quizOffWhite)
         .background(.quizGreen)
         .navigationBarBackButtonHidden()
     }
-    
-    var gradientMask: some View {
+}
+
+// MARK: - Header
+extension RankingView {
+    var Header: some View {
+        Group {
+            Image("ranking-background")
+                .resizable()
+                .scaledToFit()
+                .background(
+                    SpinningLight()
+                )
+            Text("Hall da Fama")
+                .font(Font.custom("Kickers-Regular", size: 55))
+            Text("Descubra quem brilhou no quiz!")
+                .padding(.bottom, 20)
+        }
+    }
+}
+
+// MARK: - Empty Ranking
+extension RankingView {
+    var EmptyRanking: some View {
+        Text("Cri cri cri... Ainda não temos ninguém no Hall da Fama. Crie um perfil e responda algumas perguntas para ver seu nome por aqui!")
+            .foregroundStyle(.secondary)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal)
+    }
+}
+
+// MARK: - Gradient Mask
+extension RankingView {
+    var GradientMask: some View {
         LinearGradient(
             stops: [
                 .init(color: .clear, location: 0),
@@ -80,6 +82,28 @@ struct RankingView: View {
     }
 }
 
+// MARK: - Ranking Cells
+extension RankingView {
+    var RankingCells: some View {
+        ForEach(Array(profiles.enumerated()), id: \.element.id) { (index, profile) in
+            HStack(spacing: 20) {
+                Image("pfp\(profile.icon)")
+                    .resizable()
+                    .scaledToFit()
+                Text("\(index + 1). \(profile.name)")
+                    .font(Font.custom("Kickers-Regular", size: 25))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .lineLimit(1)
+                
+                Text("\(profile.maxScore) acertos")
+                    .font(Font.custom("Kickers-Regular", size: 25))
+            }
+            .padding(.trailing)
+            .frame(height: 50)
+            .background(Capsule().stroke(.quizOffWhite, lineWidth: 2))
+        }
+    }
+}
 #Preview {
     RankingView()
 }
